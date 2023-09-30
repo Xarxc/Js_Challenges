f31a5cba42bb3b04ecf48bcb021243e02e6c46cd
= <=>
2023-09-30 13:16:09 +0100

    js_challenges

---
+++ loop1.js
@@ -0,0 +1,14 @@
+
+
+let today = "Saturday"
+if (today == "Saturday"){
+    console.log("Come to coding school")}
+    else if (today == "Sunday"){
+        console.log("Sleep")
+    }
+else if ( today == "Monday"){
+console.log("go to school")}
+else {
+    console.log("do whatever you like")
+}
+
