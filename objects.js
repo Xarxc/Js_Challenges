1170d06a42288b7b3f5e6a495f557eea677d584c
= <=>
2023-10-14 13:45:27 +0100

    seperate the files

--- objects.js
+++ objects.js
@@ -11,3 +11,28 @@ let player1 ={
  }
 }
 console.log(player1.shoot())
+
+// write a simple javascript object for a stadium
+
+
+let stadium = {
+    name: "Mohamed ahmed stadium Stadium",
+    location: "City, Country",
+    capacity: 50000,
+    opened: 2000,
+    isRoofed: true,
+    getYearsSinceOpening: function() {
+      let currentYear = new Date().getFullYear();
+      return currentYear - this.opened;
+    }
+  };
+  
+  // Accessing the properties
+  console.log(`Stadium Name: ${stadium.name}`);
+  console.log(`Location: ${stadium.location}`);
+  console.log(`Capacity: ${stadium.capacity}`);
+  console.log(`Opened: ${stadium.opened}`);
+  console.log(`Roofed: ${stadium.isRoofed}`);
+  
+  // Calculating years since opening
+  console.log(`Years Since Opening: ${stadium.getYearsSinceOpening()}`);
